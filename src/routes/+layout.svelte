95e8c42afae224064f1b3df1ecf3e80fe9c28590
J W <jonas.wienberg@hfg-gmuend.de>
2025-01-08 13:51:02 +0100

    Initial setup of SvelteKit project with configuration files and basic structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './app.css'
+</script>
+
+<svelte:head>
+	<title>Matching Game</title>
+	<meta
+		name="description"
+		content="SvelteKit is a framework for building web applications of all sizes."
+	/>
+</svelte:head>
+
+<slot />
